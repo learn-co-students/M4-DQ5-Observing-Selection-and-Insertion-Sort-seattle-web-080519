f032ce6932489d98a0ee41ba70a080e99e4902f0
Daniel Seehausen <Daniel.Seehausen@gmail.com>
2017-12-19 13:52:20 -0500

    Update insertionSort.js

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,10 +1,4 @@
 function insertionSort(arr) {
-  let startIdx = 0
-  for (let startIdx = 0; startIdx < arr.length; startIdx++) {
-    for(let currIdx = startIdx; currIdx > 0 && arr[currIdx-1] > arr[currIdx]; currIdx--) {
-      let temp = arr[currIdx]
-      arr[currIdx] = arr[currIdx-1]
-      arr[currIdx-1] = temp
-    }
-  }
+  // your code here!
+  // arr is an array of unsorted integers (i.e. [3, 5, 1])
 }
