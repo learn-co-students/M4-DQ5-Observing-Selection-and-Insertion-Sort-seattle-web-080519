190923ae773c096f68b65d1312b6d90db3e503bd
Daniel Seehausen <Daniel.Seehausen@gmail.com>
2017-12-19 13:43:40 -0500

    two solution

--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,12 @@
 function selectionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for (let oIdx = 0; oIdx < arr.length-1; oIdx++) {
+    let minIdx = oIdx
+    for (var iIdx = oIdx; iIdx < arr.length; iIdx++) {
+      if (arr[iIdx] < arr[minIdx])
+        minIdx = iIdx
+    }
+    let temp = arr[oIdx]
+    arr[oIdx] = arr[minIdx]
+    arr[minIdx] = temp
+  }
 }
